ec017a0e3369a1469af550238c5f3cddce255e91
Eemeli Miettinen <eemeli.miettinen@methics.fi>
2024-01-12 16:30:27 +0200

    MUSAP-117: Added support for updating FCM token

--- app/src/main/java/fi/methics/divvy/DivvyFirebaseService.java
+++ app/src/main/java/fi/methics/divvy/DivvyFirebaseService.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import fi.methics.musap.sdk.api.MusapClient;
 import fi.methics.musap.sdk.internal.util.MLog;
 
 public class DivvyFirebaseService extends FirebaseMessagingService {
@@ -12,7 +13,7 @@ public class DivvyFirebaseService extends FirebaseMessagingService {
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
-        MLog.d("New token", token);
+        MusapClient.updateFcmToken(token, null);
     }
 
 }
