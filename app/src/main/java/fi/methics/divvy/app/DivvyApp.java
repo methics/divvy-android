c1048da8c7567fa1dc7564b78d47ce275be0ad30
Eemeli Miettinen <eemeli.miettinen@methics.fi>
2024-01-15 16:00:13 +0200

    Fix MUSAP Link URL

--- app/src/main/java/fi/methics/divvy/app/DivvyApp.java
+++ app/src/main/java/fi/methics/divvy/app/DivvyApp.java
@@ -17,7 +17,7 @@ import fi.methics.musap.sdk.sscd.yubikey.YubiKeyOpenPgpSscd;
  * This must also be declared in the manifest.
  */
 public class DivvyApp extends Application {
-    public static final String LINK_URL = "https://demo.methics.fi/musapdemo/";
+    public static final String LINK_URL = "https://demo.methics.fi/musap/";
 
     @Override
     public void onCreate() {
